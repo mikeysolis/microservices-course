f585821090903cd4afd13dde494c9ebcdfdafb85
Mike Solis <mikey@Mikes-iMac.local>
2022-06-27 16:51:41 -1000

    Basic apis completed, part 6 completed

---
+++ CommandsService/Models/CommandCreateDto.cs
@@ -0,0 +1,7 @@
+namespace CommandsService.Models;
+
+public class CommandCreateDto
+{
+    public string? HowTo { get; set; }
+    public string? CommandLine { get; set; }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -6,8 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<AppDbContext>(opt => 
-    opt.UseInMemoryDatabase("InMem"));
+
+
+if (builder.Environment.IsProduction())
+{
+    Console.WriteLine("--> Using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(opt => 
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine("--> Using InMem Db");
+    builder.Services.AddDbContext<AppDbContext>(opt => 
+        opt.UseInMemoryDatabase("InMem"));
+}
+
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddControllers();
@@ -34,6 +47,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-PrepDb.PrepPopulation(app);
+PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 
 app.Run();
